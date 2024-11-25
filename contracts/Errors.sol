8d8637be2d50667149afd2055c1c30b0041d5913
AshiqAhamed17 <aashiq121205@gmail.com>
2024-11-25 05:42:01 +0530

    update Errors.sol

--- contracts/Errors.sol
+++ contracts/Errors.sol
@@ -3,8 +3,26 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 contract Errors {
+    int amount;
+    uint8 age;
 
-    function testRequire(uint a) public pure {
-        
+    function testRequire(int _amount) public {
+        require(_amount > 0, "the amount must be bigger than 0");
+        amount += _amount;
     }
+
+    function setAge(uint8 _age) public {
+        if(_age < 18){
+            revert("you're too young!");
+        }
+        age = _age;
+    }
+
+    function testAssert() public view {
+        assert(amount > 100);
+    }
+
+
+
+
 }
