91bedc8e8dac2ff8632085a4fa27739f665286c8
AshiqAhamed17 <aashiq121205@gmail.com>
2025-01-12 10:22:54 +0530

    Update Delegatecall.sol

--- contracts/2_Intermediate/2_Delegatecall.sol
+++ contracts/2_Intermediate/2_Delegatecall.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.2 <0.9.0;
 
-contract Testdelegatecall {
+//Logic Contract
+contract TestdelegatecallV1 {
     // NOTE: storage layout must be the same as contract Delegatecall
     uint public num;
     address public sender;
@@ -14,6 +15,22 @@ contract Testdelegatecall {
     }
 }
 
+//Logic contract 2
+contract TestdelegatecallV2 {
+    uint public num;
+    address public sender;
+    uint public value;
+
+    function setValue(uint _num) external payable {
+        num = 10 * _num;
+        sender = msg.sender;
+        value = msg.value;
+    }
+
+}
+
+
+//Contract which delegate call the logic contracrt with context of this contract
 contract Delegatecall {
     uint public num;
     address public sender;
@@ -21,23 +38,27 @@ contract Delegatecall {
 
     event DelegateResponse(bool success, bytes data);
     function setValue(address _test, uint _num) external payable {
-        /*
+        
         (bool success, bytes memory data) = _test.delegatecall(
-            abi.encodeWithSignature("setValue(uint256)", _num);
-        )
-        */
-        // Delegatecall's storage is set; Testdelegatecall's storage is not modified.
-        (bool success, bytes memory data) = _test.delegatecall(
-            abi.encodeWithSelector(Testdelegatecall.setValue.selector, _num)
+            abi.encodeWithSignature("setValue(uint256)", _num)
         );
+    
+        // Delegatecall's storage is set; Testdelegatecall's storage is not modified.
+        /*(bool success, bytes memory data) = _test.delegatecall(
+            abi.encodeWithSelector(Testdelegatecall.setValue.selector, _num)
+        );*/
         require(success, "Delegatecall Failed !!!!");
         emit DelegateResponse(success, data);
     }
 
     // Testdelegatecall's storage is set; Delegatecall's storage is not modified.
     function setValueCall(address _test, uint _num) external payable {
-        (bool success, bytes memory data) = _test.call{value: msg.value}(
+        /*(bool success, bytes memory data) = _test.call{value: msg.value}(
             abi.encodeWithSelector(Testdelegatecall.setValue.selector, _num)
+        );*/
+
+        (bool success, bytes memory data) = _test.call{value: msg.value}(
+            abi.encodeWithSignature("setValue(uint256)", _num)
         );
         require(success, "Delegatecall Failed !!!!");
         emit DelegateResponse(success, data);
