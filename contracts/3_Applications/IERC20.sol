7711b16bf9ab6f55e23dc3e08ba18d3e786d3e7f
AshiqAhamed17 <aashiq121205@gmail.com>
2025-01-14 22:36:34 +0530

    update ERC20.sol

--- contracts/3_Applications/IERC20.sol
+++ contracts/3_Applications/IERC20.sol
@@ -14,11 +14,5 @@ interface IERC20 {
 
     function transferFrom(address sender, address recipient, uint amount) external returns (bool);
 
-    event Transfer(address indexed from, address indexed to, uint amount);
-    event Approval(address indexed owner, address indexed spender, uint amount);
-
 }
 
-contract ERC20 is IERC20 {
-    
-}
